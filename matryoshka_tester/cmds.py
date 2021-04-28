f925c0c189fcee50853972b81b297fa1c213e0a8
Jean-Philippe Evrard <jean-philippe.evrard@suse.com>
2021-04-28 09:31:39 +0200

    Update packaging

---
+++ matryoshka_tester/cmds.py
@@ -0,0 +1,12 @@
+from pprint import pprint
+
+from matryoshka_tester.data import containers
+
+def fetch():
+    pass
+
+def run():
+    pass
+
+def fetch_all():
+    pprint(containers)
---
+++ matryoshka_tester/data.py
@@ -0,0 +1,9 @@
+containers = dict()
+containers['go'] = {
+    '1.16': 'registry.opensuse.org/home/fcrozat/matryoshka/containerfile/opensuse/golang:1.16',
+    '1.15': 'registry.opensuse.org/home/fcrozat/matryoshka/containerfile/opensuse/golang:1.15',   
+}
+containers['openjdk'] = {
+    '11': 'registry.opensuse.org/home/fcrozat/matryoshka/containerfile/opensuse/openjdk:11',
+    '16': 'registry.opensuse.org/home/fcrozat/matryoshka/containerfile/opensuse/openjdk:16',
+}
