3b67eb354705e93350d33eb97a254c24eb8f6924
Jean-Philippe Evrard <jean-philippe.evrard@suse.com>
2021-04-28 14:03:11 +0200

    Cleanup for parallelization
    Now using asyncio because I couldn't wait.

--- matryoshka_tester/cmds.py
+++ matryoshka_tester/cmds.py
@@ -1,62 +1,43 @@
 import argparse
 import subprocess
+import asyncio
 
 from matryoshka_tester.data import containers
 
+from prettytable import PrettyTable
 
 # Do real stuff
-def pull_container(url):
+async def pull_container(url):
     """ Pulls the container given in url with docker CLI """
-    subprocess.check_call(["docker", "pull", url])
+    process = await asyncio.create_subprocess_shell(
+        "docker pull {}".format(url), stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    print(f'[stdout]\n{stdout.decode().strip()}')
+    print(f'[stderr]\n{stderr.decode().strip()}')
+
+# CLI
+def list_containers():
+    pt = PrettyTable()
+    pt.field_names = ['Language', 'Version', 'URL']
+    pt.align = "l"
+    for language, versionsdict in containers.items():
+        for version in versionsdict:
+            pt.add_row([language, version, versionsdict[version]])
+    print(pt)
+
+async def fetch_containers(all_containers=False, container_type=''):
+    for language, versionsdict in containers.items():
+        if language == container_type or all_containers:
+            for version in versionsdict:
+                await asyncio.gather(pull_container(versionsdict[version]))
+
+def fetch_all_containers():
+    asyncio.run(fetch_containers(all_containers=True))
 
 
-# DRY
-def per_containertype(method):
+def fetch_language_containers():
     parser = argparse.ArgumentParser()
-    parser.add_argument("container_type", help="One of the container types")
+    parser.add_argument("language", help="language/container type")
     arg = parser.parse_args()
-    if arg.container_type in containers:
-        for version in containers[arg.container_type]:
-            method(arg.container_type, version)
-
-
-def list_containertype_versions(containertype, version):
-    print(version)
-
-
-def fetch_versions(containertype, version):
-    pull_container(containers[containertype][version])
-
-
-# Convenience CLI tools
-def list_container_types():
-    for key in containers.keys():
-        print(key)
-
-
-def list_all_container_versions_per_containertype():
-    per_containertype(list_containertype_versions)
-
-
-def fetch_all_containers_per_containertype():
-    per_containertype(fetch_versions)
-
-
-def fetch_container():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("container_type", help="One of the container types")
-    parser.add_argument("container_version", help="One of the container versions for the container type")
-    args = parser.parse_args()
-    url = containers.get(args.container_type, {}).get(args.container_version, "")
-    if url != "":
-        pull_container(url)
-
-
-def run():
-    pass
-
-
-
-
-def run_on_all_containertype_versions(containertype, method):
-    pass
+    asyncio.run(fetch_containers(container_type=arg.language))
