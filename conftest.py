05e11fffc443850f3f0b7645d7460e7805465f3b
Jean-Philippe Evrard <jean-philippe.evrard@suse.com>
2021-04-28 19:11:37 +0200

    Cleanup further
    Well, I feel like this was too complicated, so I made it simpler
    for the end user writing tests.
    
    Hope it helps.

---
+++ conftest.py
@@ -0,0 +1,46 @@
+import pytest
+import testinfra
+import subprocess
+import os
+
+from collections import namedtuple
+from matryoshka_tester.data import containers
+
+ContainerData = namedtuple("Container", ["version", "image", "connection"])
+
+
+@pytest.fixture(scope="module")
+def container(request):
+    docker_id = (
+        subprocess.check_output(
+            ["docker", "run", "-d", "-it", request.param[1], "/bin/sh"]
+        )
+        .decode()
+        .strip()
+    )
+    yield ContainerData(*request.param, testinfra.get_host("docker://" + docker_id))
+    subprocess.check_call(["docker", "rm", "-f", docker_id])
+
+
+def pytest_generate_tests(metafunc):
+    # Finds container_type.
+    # If necessary, you can override the detection by setting a variable "container_type" in your module.
+    container_type = getattr(metafunc.module, "container_type", "")
+    if container_type == "":
+        container_type = (
+            os.path.basename(metafunc.module.__file__)
+            .strip()
+            .replace("test_", "")
+            .replace(".py", "")
+        )
+
+    if "container" in metafunc.fixturenames:
+        metafunc.parametrize(
+            "container",
+            [
+                (ver, containers[container_type][ver])
+                for ver in containers[container_type]
+            ],
+            ids=[ver for ver in containers[container_type]],
+            indirect=True,
+        )
--- test_openjdk.py
+++ test_openjdk.py
@@ -1,65 +1,12 @@
 import pytest
-import subprocess
 import testinfra
 
-# def pytest_generate_tests(metafunc):
-#     metafunc.parametrize("version_tag",  metafunc.cls.jdks, scope="class")
-class MyTester:
-    def __init__(self, jdk):
-        self.jdk_version = jdk[0]
-        self.container_url = jdk[1]
 
-    def get_container(self):
-        subprocess.check_call(
-            ["echo", "woot", "|", "wall", "&&", "docker", "pull", self.container_url]
-        )
-        docker_id = (
-            subprocess.check_output(
-                ["docker", "run", "-d", "-it", self.container_url, "/bin/sh"]
-            )
-            .decode()
-            .strip()
-        )
-        yield testinfra.get_host("docker://" + docker_id)
-        subprocess.check_call(["docker", "rm", "-f", docker_id])
+def test_jdk_version(container):
+    assert "openjdk {}".format(container.version) in container.connection.check_output(
+        "java --version"
+    )
 
 
-@pytest.fixture(scope="module")
-def tester(request):
-    """Create a container from containerurl, returns docker_url"""
-    return MyTester(request.param)
-
-
-class TestWithDifferentJDKs:
-    jdks = [
-        (
-            "11",
-            "registry.opensuse.org/home/fcrozat/matryoshka/containerfile/opensuse/openjdk:11",
-        ),
-        (
-            "16",
-            "registry.opensuse.org/home/fcrozat/matryoshka/containerfile/opensuse/openjdk:16",
-        ),
-    ]
-
-    @pytest.mark.parametrize("tester", jdks, indirect=["tester"])
-    def test_jdk_version(self, tester):
-        host = next(tester.get_container())
-        assert "openjdk {}".format(tester.jdk_version) in host.check_output(
-            "java --version"
-        )
-
-    @pytest.mark.parametrize("tester", jdks, indirect=["tester"])
-    def test_maven_present(self, tester):
-        host = next(tester.get_container())
-        assert host.run_expect([0], "mvn --version")
-
-    @pytest.mark.parametrize("tester", jdks, indirect=["tester"])
-    def test_maven_present2(self, tester):
-        host = next(tester.get_container())
-        assert host.run_expect([0], "mvn --version")
-
-    @pytest.mark.parametrize("tester", jdks, indirect=["tester"])
-    def test_maven_present3(self, tester):
-        host = next(tester.get_container())
-        assert host.run_expect([0], "mvn --version")
+def test_maven_present(container):
+    assert container.connection.run_expect([0], "mvn --version")
