5973efe85bb93f9d926cb83e7735c986d5e9ffd9
Jean-Philippe Evrard <jean-philippe.evrard@suse.com>
2021-04-29 14:58:32 +0200

    Add extra js packages to test

--- test_node.py
+++ test_node.py
@@ -21,13 +21,108 @@ def test_npm(container):
     assert container.connection.run_expect([0], "npm version")
 
 
-@restrict_to_version(["14"])
+#@restrict_to_version(["14"])
 def test_lodash(container):
     cmd = container.connection.run(
         """git clone https://github.com/lodash/lodash.git &&
         cd lodash &&
         npm ci &&
         npm test
+        """
+    )
+    print(cmd.stdout)
+    assert cmd.rc == 0
+
+# @restrict_to_version(["14"])
+def test_chalk(container):
+    cmd = container.connection.run(
+        """git clone https://github.com/chalk/chalk.git &&
+        cd chalk &&
+        npm install &&
+        npm test
+        """
+    )
+    print(cmd.stdout)
+    assert cmd.rc == 0
+
+
+def test_commanderjs(container):
+    cmd = container.connection.run(
+        """git clone https://github.com/tj/commander.js.git &&
+        cd commander.js &&
+        npm ci &&
+        npm test &&
+        npm run lint
+        """
+    )
+    print(cmd.stdout)
+    assert cmd.rc == 0
+
+
+def test_visionmediadebug(container):
+    cmd = container.connection.run(
+        """git clone https://github.com/visionmedia/debug.git &&
+        cd debug &&
+        npm install &&
+        npm run lint &&
+        npm run test:node
+        """
+    )
+    print(cmd.stdout)
+    assert cmd.rc == 0
+
+
+def test_express(container):
+    cmd = container.connection.run(
+        """git clone https://github.com/expressjs/express.git &&
+        cd express &&
+        npm config set shrinkwrap false &&
+        npm rm --silent --save-dev connect-redis &&
+        npm run test-ci &&
+        npm run lint
+        """
+    )
+    print(cmd.stdout)
+    assert cmd.rc == 0
+
+
+@pytest.mark.xfail(
+    reason="https://github.com/jprichardson/node-fs-extra/issues/898"
+)
+def test_nodefsextra(container):
+    cmd = container.connection.run(
+        """git clone https://github.com/jprichardson/node-fs-extra.git &&
+        cd node-fs-extra &&
+        npm install &&
+        npm run unit
+        """
+    )
+    print(cmd.stdout)
+    assert cmd.rc == 0
+
+
+def test_moment(container):
+    cmd = container.connection.run(
+        """git clone https://github.com/moment/moment.git &&
+        cd moment &&
+        npm install -g grunt-cli &&
+        npm install &&
+        grunt test
+        """
+    )
+    print(cmd.stdout)
+    assert cmd.rc == 0
+
+
+def test_proptypes(container):
+    cmd = container.connection.run(
+        """git clone https://github.com/facebook/prop-types.git &&
+        cd prop-types &&
+        yarn install &&
+        npm install --no-save "react@~16" "react-dom@~16" &&
+        yarn run pretest &&
+        yarn run tests-only &&
+        yarn run build
         """
     )
     print(cmd.stdout)
