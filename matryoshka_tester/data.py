53e8ae4bb814292803be7579f50edd65b9a17844
Jean-Philippe Evrard <jean-philippe.evrard@suse.com>
2021-04-29 12:47:49 +0200

    Update matryoshka urls and increase coverage
    Dan updated the images and moved them to fcrozat's home.
    This adapts the location of the containers, ensure
    the latest containers are always fetched, and increase the
    amount of containers to test.

--- matryoshka_tester/data.py
+++ matryoshka_tester/data.py
@@ -4,12 +4,16 @@ containers["go"] = {
     "1.15": "registry.opensuse.org/home/fcrozat/matryoshka/containerfile/opensuse/golang:1.15",
 }
 containers["node"] = {
-    "15": "registry.opensuse.org/home/dancermak/nodejs/containers_node15/node15:latest",
+    "15": "registry.opensuse.org/home/fcrozat/matryoshka/containerfile/node15:latest",
+    "14": "registry.opensuse.org/home/fcrozat/matryoshka/containerfile/node14:latest",
+    "12": "registry.opensuse.org/home/fcrozat/matryoshka/containerfile/node12:latest",
+    "10": "registry.opensuse.org/home/fcrozat/matryoshka/containerfile/node10:latest",
 }
 containers["openjdk"] = {
-    "11": "registry.opensuse.org/home/fcrozat/matryoshka/containerfile/opensuse/openjdk:11",
     "16": "registry.opensuse.org/home/fcrozat/matryoshka/containerfile/opensuse/openjdk:16",
+    "11": "registry.opensuse.org/home/fcrozat/matryoshka/containerfile/opensuse/openjdk:11",
 }
 containers["python"] = {
-    "3.9": "registry.opensuse.org/home/dancermak/python/containers_python39/python39:latest",
+    "3.9": "registry.opensuse.org/home/fcrozat/matryoshka/containerfile/python39:latest",
+    "3.8": "registry.opensuse.org/home/fcrozat/matryoshka/containerfile/python38:latest",
 }
