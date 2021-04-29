1ee68a3ebd4318837ab3a1741d871e473c4055a9
Jean-Philippe Evrard <jean-philippe.evrard@suse.com>
2021-04-29 12:04:20 +0200

    Build kured with tooling
    This will allow us to test a very simple project, which uses
    golang, prometheus, and kubernetes clients.

--- test_go.py
+++ test_go.py
@@ -4,3 +4,14 @@ import testinfra
 
 def test_go_version(container):
     assert container.version in container.connection.check_output("go version")
+
+
+def test_kured(container):
+    cmd = container.connection.run(
+        """git clone https://github.com/weaveworks/kured.git &&
+        cd kured &&
+        make cmd/kured/kured
+        """
+    )
+    print(cmd.stdout)
+    assert cmd.rc == 0
