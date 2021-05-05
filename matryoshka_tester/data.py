627e8dbdae1988aae99e2902e14066933866a034
Marco Varlese <marco.varlese@suse.com>
2021-05-05 09:06:08 +0200

    Updated Java & Go containers naming convention and registry urls

--- matryoshka_tester/data.py
+++ matryoshka_tester/data.py
@@ -1,7 +1,7 @@
 containers = dict()
 containers["go"] = {
-    "1.16": "registry.opensuse.org/home/fcrozat/matryoshka/containerfile/opensuse/golang:1.16",
-    "1.15": "registry.opensuse.org/home/fcrozat/matryoshka/containerfile/opensuse/golang:1.15",
+    "1.16": "registry.opensuse.org/home/fcrozat/matryoshka/containerfile/bci/golang:1.16",
+    "1.15": "registry.opensuse.org/home/fcrozat/matryoshka/containerfile/bci/golang:1.15",
 }
 containers["node"] = {
     "15": "registry.opensuse.org/home/fcrozat/matryoshka/containers_node15/node15:latest",
@@ -9,8 +9,10 @@ containers["node"] = {
     "12": "registry.opensuse.org/home/fcrozat/matryoshka/containers_node12/node12:latest",
 }
 containers["openjdk"] = {
-    "16": "registry.opensuse.org/home/fcrozat/matryoshka/containerfile/opensuse/openjdk:16",
-    "11": "registry.opensuse.org/home/fcrozat/matryoshka/containerfile/opensuse/openjdk:11",
+    "16": "registry.opensuse.org/home/fcrozat/matryoshka/containerfile/bci/openjdk:16",
+    "15": "registry.opensuse.org/home/fcrozat/matryoshka/containerfile/bci/openjdk:15",
+    "14": "registry.opensuse.org/home/fcrozat/matryoshka/containerfile/bci/openjdk:14",
+    "11": "registry.opensuse.org/home/fcrozat/matryoshka/containerfile/bci/openjdk:11",
 }
 containers["python"] = {
     "3.9": "registry.opensuse.org/home/fcrozat/matryoshka/containers_python39/python39:latest",
