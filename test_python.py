65bb36a28b2abd24fe062c45c82944f854443e3d
Dan Čermák <dcermak@suse.com>
2021-06-02 15:57:15 +0200

    Add checks for the python container environment variables

--- test_python.py
+++ test_python.py
@@ -1,12 +1,37 @@
 def test_python_version(auto_container):
-    assert auto_container.version in auto_container.connection.check_output(
-        "python --version"
+    # output is: Python $version
+    version_output = auto_container.connection.check_output("python --version")
+    py_version = version_output.split()[1]
+    assert auto_container.version in py_version
+    assert (
+        auto_container.connection.check_output("echo $PYTHON_VERSION")
+        == py_version
     )
 
 
-# We don't care about the version, just test that the command seem to work
 def test_pip(auto_container):
-    assert auto_container.connection.run_expect([0], "pip --version")
+    # output is: pip $PIP_VER from $PATH (python $PYTHON_VER)
+    res = auto_container.connection.run_expect([0], "pip --version")
+    pip_ver_from_env = auto_container.connection.check_output(
+        "echo $PIP_VERSION"
+    )
+    entries = res.stdout.strip().split()
+
+    assert entries[0] == "pip", (
+        "Unexpected first entry in the output of 'pip --version', "
+        f"expected 'pip'. got {entries[0]}"
+    )
+
+    assert entries[1] == pip_ver_from_env, (
+        f"Got a different pip version from ENV ({pip_ver_from_env}) "
+        f"than from pip ({entries[1]})"
+    )
+
+    py_ver_of_pip = entries[-1].replace(")", "")
+    assert py_ver_of_pip == auto_container.version, (
+        f"pip is installed for python {py_ver_of_pip} but this is a "
+        f"python {auto_container.version} container"
+    )
 
 
 # run pip check
@@ -16,4 +41,4 @@ def test_recent_pip(auto_container):
 
 # Ensure we can pip install tox
 def test_tox(auto_container):
-    assert auto_container.connection.run("pip install --user tox").rc == 0
+    auto_container.connection.run_expect([0], "pip install --user tox")
