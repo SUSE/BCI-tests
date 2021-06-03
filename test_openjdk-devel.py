279d20bf9fc60b1f02d40b0dcf680374b89147c9
Dan Čermák <dcermak@suse.com>
2021-06-03 12:04:24 +0200

    Add test for the openjdk-devel entrypoint

--- test_openjdk-devel.py
+++ test_openjdk-devel.py
@@ -1,3 +1,8 @@
+import pytest
+
+from matryoshka_tester.parse_data import containers
+
+
 def test_jdk_version(auto_container):
     assert (
         f"javac {auto_container.version}"
@@ -10,3 +15,24 @@ def test_jdk_version(auto_container):
 
 def test_maven_present(auto_container):
     assert auto_container.connection.run_expect([0], "mvn --version")
+
+
+OPENJDK_DEVEL_CONTAINERS = [
+    (container.url, container.version)
+    for container in containers
+    if container.type == "openjdk-devel"
+]
+
+
+@pytest.mark.parametrize(
+    "devel_container",
+    OPENJDK_DEVEL_CONTAINERS,
+)
+def test_entrypoint(devel_container, host, container_runtime):
+    intro = host.run_expect(
+        [0],
+        f"{container_runtime.runner_binary} run --rm {devel_container[0]}",
+    ).stdout
+
+    assert "jshell>" in intro
+    assert f"Welcome to JShell -- Version {devel_container[1]}" in intro
