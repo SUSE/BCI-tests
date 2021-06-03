831ca497c8b352a2cb997561c0b9bd23c032f770
Dan Čermák <dcermak@suse.com>
2021-06-03 11:58:48 +0200

    Add tests for the environment variables exposed by openjdk & go containers

--- test_openjdk.py
+++ test_openjdk.py
@@ -2,3 +2,7 @@ def test_jdk_version(auto_container):
     assert "openjdk {}".format(
         auto_container.version
     ) in auto_container.connection.check_output("java --version")
+
+    assert auto_container.version == auto_container.connection.check_output(
+        "echo $JAVA_VERSION"
+    )
