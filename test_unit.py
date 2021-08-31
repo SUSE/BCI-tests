3acb9fd0144d80cd497af0a8cf4d1f5ecbf6b73f
Dan Čermák <dcermak@suse.com>
2021-08-31 15:51:59 +0200

    Remove now obsolete commands

--- test_unit.py
+++ test_unit.py
@@ -1,20 +1,4 @@
-import pytest
-
-from bci_tester.parse_data import build_containerlist, Container
 from bci_tester.fips import host_fips_enabled, host_fips_supported
-
-def test_default_containerlist():
-    # assert len makes sure we are reading the default file
-    # to check for any eventual TypeErrors
-    assert len(build_containerlist()) != 0
-
-
-def test_buildcontainerlist(tmp_path):
-    tmpfile = tmp_path / "temp.json"
-    with open(tmpfile, "w") as fw:
-        fw.write("{}")
-    with pytest.raises(TypeError):
-        build_containerlist(tmpfile)
 
 
 def test_host_fips_supported(tmp_path):
