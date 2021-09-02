4961f5418cb909d01b4bb0a523188b691c437bc4
Dan Čermák <dcermak@suse.com>
2021-09-02 17:49:51 +0200

    Add some basic tests for the dotnet containers

---
+++ test_dotnet.py
@@ -0,0 +1,85 @@
+import pytest
+from bci_tester.data import DOTNET_ASPNET_3_1_BASE_CONTAINER
+from bci_tester.data import DOTNET_ASPNET_5_0_BASE_CONTAINER
+from bci_tester.data import DOTNET_SDK_3_1_BASE_CONTAINER
+from bci_tester.data import DOTNET_SDK_5_0_BASE_CONTAINER
+from bci_tester.helpers import GitRepositoryBuild
+
+
+CONTAINER_IMAGES = [
+    DOTNET_SDK_3_1_BASE_CONTAINER,
+    DOTNET_SDK_5_0_BASE_CONTAINER,
+]
+
+
+@pytest.mark.parametrize(
+    "container,sdk_version",
+    [
+        (DOTNET_SDK_3_1_BASE_CONTAINER, "3.1"),
+        (DOTNET_SDK_5_0_BASE_CONTAINER, "5.0"),
+    ],
+    indirect=["container"],
+)
+def test_dotnet_sdk_version(container, sdk_version):
+    assert (
+        container.connection.check_output("dotnet --list-sdks")[:3]
+        == sdk_version
+    )
+    runtimes = container.connection.check_output("dotnet --list-runtimes")
+    assert ("Microsoft.AspNetCore.App " + sdk_version) in runtimes
+    assert ("Microsoft.NETCore.App " + sdk_version) in runtimes
+
+
+@pytest.mark.parametrize(
+    "container,sdk_version",
+    [
+        (DOTNET_ASPNET_3_1_BASE_CONTAINER, "3.1"),
+        (DOTNET_ASPNET_5_0_BASE_CONTAINER, "5.0"),
+    ],
+    indirect=["container"],
+)
+def test_dotnet_aspnet_version(container, sdk_version):
+    assert container.connection.check_output("dotnet --list-sdks") == ""
+    runtimes = container.connection.check_output("dotnet --list-runtimes")
+    assert ("Microsoft.AspNetCore.App " + sdk_version) in runtimes
+    assert ("Microsoft.NETCore.App " + sdk_version) in runtimes
+
+
+@pytest.mark.parametrize(
+    "container",
+    [
+        pytest.param(
+            DOTNET_SDK_3_1_BASE_CONTAINER,
+            marks=pytest.mark.xfail(
+                reason="NETCore version mismatch, expected 3.1.16, got 3.1.9"
+            ),
+        ),
+        DOTNET_SDK_5_0_BASE_CONTAINER,
+    ],
+    indirect=["container"],
+)
+def test_dotnet_hello_world(container):
+    container.connection.run_expect([0], "dotnet new console -o MyApp")
+    assert (
+        container.connection.check_output("cd MyApp && dotnet run")
+        == "Hello World!"
+    )
+
+
+@pytest.mark.parametrize(
+    "container", [DOTNET_SDK_5_0_BASE_CONTAINER], indirect=["container"]
+)
+@pytest.mark.parametrize(
+    "container_git_clone",
+    [
+        GitRepositoryBuild(
+            repository_url="https://github.com/nopSolutions/nopCommerce.git",
+            repository_tag="release-4.40.4",
+            build_command="""dotnet build ./src/NopCommerce.sln &&
+dotnet test ./src/Tests/Nop.Tests/Nop.Tests.csproj""",
+        )
+    ],
+    indirect=["container_git_clone"],
+)
+def test_popular_web_apps(container, container_git_clone):
+    container.connection.run_expect([0], container_git_clone.test_command)
