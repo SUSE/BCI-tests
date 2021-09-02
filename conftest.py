01f42beca8c872913f9bbb84163c69b6b2293e06
Dan Čermák <dcermak@suse.com>
2021-09-02 13:49:51 +0200

    Update fixtures to work with the new test setup

--- conftest.py
+++ conftest.py
@@ -1,39 +1,55 @@
+import asyncio
+import functools
 import pytest
 import testinfra
-import subprocess
 import os
 import tempfile
 
-from typing import Any, NamedTuple
+from typing import Any, NamedTuple, Optional, Union
 
 from requests import get
 
-from bci_tester.parse_data import Container
+from bci_tester.data import Container, DerivedContainer
 from bci_tester.helpers import (
+    check_output,
     get_selected_runtime,
     GitRepositoryBuild,
 )
 
 
 class ContainerData(NamedTuple):
-    version: str
     image_url: str
+    container_id: str
     connection: Any
 
 
 @pytest.fixture(scope="function")
-def container_git_clone(request, auto_container):
+def container_git_clone(request):
     """This fixture clones the `GitRepositoryBuild` passed as an indirect
     parameter to it into the currently selected container.
 
     It returns the GitRepositoryBuild to the requesting test function.
     """
-    assert isinstance(request.param, GitRepositoryBuild), (
-        f"got an invalid request parameter {type(request.param)}, "
-        "expected GitRepository"
-    )
-    auto_container.connection.run_expect([0], request.param.clone_command)
-    yield request.param
+    if isinstance(request.param, GitRepositoryBuild):
+        git_repo_build = request.param
+    elif (
+        hasattr(request.param, "values")
+        and len(request.param.values) == 1
+        and isinstance(request.param.values[0], GitRepositoryBuild)
+    ):
+        git_repo_build = request.param.values[0]
+    else:
+        raise ValueError(
+            f"got an invalid request parameter {type(request.param)}, "
+            "expected GitRepositoryBuild or SubRequest with a GitRepositoryBuild"
+        )
+
+    if "container" in request.fixturenames:
+        cont = request.getfixturevalue("container")
+    else:
+        cont = request.getfixturevalue("auto_container")
+    cont.connection.run_expect([0], git_repo_build.clone_command)
+    yield git_repo_build
 
 
 @pytest.fixture(scope="function")
@@ -70,36 +86,65 @@ def container_runtime():
     return get_selected_runtime()
 
 
-# All the tests using this fixture in a single testfile will use single container,
-# unless the parallelization effort forces them to create another one.
-# If we need to move towards one test per container, remove
-# scope='module' to default back to scope='function'
-# NB: We've pulled the image beforehand, so the docker-run should be almost instant, and it shouldn't be a problem
-# to switch scope. If the docker pull is _NOT_ in the tox.ini, make sure your pull the image if you want to run on scope='function'.
 @pytest.fixture(scope="module")
-def auto_container(request, container_runtime):
-    launch_data: Container = request.param
+def event_loop():
+    return asyncio.get_event_loop()
 
-    container_id = (
-        subprocess.check_output(
+
+@pytest.fixture(scope="module")
+async def auto_container(request, container_runtime):
+    """Fixture that will build & launch a container that is either passed as a
+    request parameter or it will be automatically parametrized via
+    pytest_generate_tests.
+    """
+    launch_data: Union[Container, DerivedContainer] = request.param
+
+    container_id: Optional[str] = None
+    try:
+        await launch_data.prepare_container()
+        container_id = await check_output(
             [container_runtime.runner_binary] + launch_data.launch_cmd
         )
-        .decode()
-        .strip()
-    )
-    yield ContainerData(
-        version=launch_data.version,
-        image_url=launch_data.url,
-        connection=testinfra.get_host(
-            f"{container_runtime.runner_binary}://{container_id}"
-        ),
-    )
-    subprocess.check_call(
-        [container_runtime.runner_binary, "rm", "-f", container_id]
-    )
+        yield ContainerData(
+            image_url=launch_data.url,
+            container_id=launch_data.container_id,
+            connection=testinfra.get_host(
+                f"{container_runtime.runner_binary}://{container_id}"
+            ),
+        )
+    except RuntimeError as exc:
+        raise exc
+    finally:
+        if container_id is not None:
+            await check_output(
+                [container_runtime.runner_binary, "rm", "-f", container_id]
+            )
 
 
 container = auto_container
+
+
+def pytest_generate_tests(metafunc):
+    if "auto_container" in metafunc.fixturenames and (
+        (container_img := getattr(metafunc.module, "CONTAINER_IMAGE", None))
+        is not None
+        or (
+            container_images := getattr(
+                metafunc.module, "CONTAINER_IMAGES", None
+            )
+        )
+        is not None
+    ):
+
+        if container_img is not None:
+            metafunc.parametrize(
+                "auto_container", [container_img], indirect=True
+            )
+        else:
+            assert container_images is not None
+            metafunc.parametrize(
+                "auto_container", container_images, indirect=True
+            )
 
 
 @pytest.fixture(scope="module")
@@ -134,3 +179,28 @@ def dapper(host):
             with open(dest, "wb") as dapper_file:
                 dapper_file.write(resp.content)
             yield dest
+
+
+def restrict_to_containers(containers):
+    def inner(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            c = kwargs.get("auto_container")
+            try:
+                len(containers)
+            except TypeError:
+                urls = [containers.url]
+            else:
+                urls = [cont.url for cont in containers]
+
+            # raise Exception(c.image_url, urls)
+            if c is not None and c.image_url in urls:
+                return func(*args, **kwargs)
+
+            return pytest.skip(
+                "Test is being restricted to the containers " + ", ".join(urls)
+            )
+
+        return wrapper
+
+    return inner
