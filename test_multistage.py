7234356e3e02c8385b697a0e94dc1bbf5f1b272b
Dan Čermák <dcermak@suse.com>
2021-09-23 14:38:29 +0200

    Add multstage build of the k8s-sample .Net application

--- test_multistage.py
+++ test_multistage.py
@@ -7,11 +7,14 @@ from typing import Union
 import pytest
 from bci_tester.data import Container
 from bci_tester.data import DerivedContainer
+from bci_tester.data import DOTNET_ASPNET_5_0_BASE_CONTAINER
+from bci_tester.data import DOTNET_SDK_5_0_BASE_CONTAINER
 from bci_tester.data import EXTRA_BUILD_ARGS
 from bci_tester.data import GO_1_16_CONTAINER
 from bci_tester.data import OPENJDK_BASE_CONTAINER
 from bci_tester.data import OPENJDK_DEVEL_BASE_CONTAINER
 from bci_tester.helpers import GitRepositoryBuild
+from bci_tester.helpers import LOCALHOST
 
 
 @dataclass
@@ -133,6 +136,64 @@ CMD ["/k3sup/k3sup"]
             ),
             0,
             'Use "k3sup [command] --help" for more information about a command.',
+        ),
+        pytest.param(
+            GitRepositoryBuild(
+                repository_url="https://github.com/phillipsj/adventureworks-k8s-sample.git"
+            ),
+            MultiStageBuild(
+                builder=DOTNET_SDK_5_0_BASE_CONTAINER,
+                runner=DOTNET_ASPNET_5_0_BASE_CONTAINER,
+                # modified version of upstream's Dockerfile:
+                # - the entrypoint.sh is custom, so that the application
+                #   terminates
+                # - the docker build is not run from the repos top level dir,
+                #   but one directory "above"
+                dockerfile_template=r"""FROM $builder AS build
+WORKDIR /src
+
+COPY ./adventureworks-k8s-sample/AdventureWorks.sln AdventureWorks.sln
+COPY ./adventureworks-k8s-sample/AdventureWorks.App/*.csproj ./AdventureWorks.App/
+COPY ./adventureworks-k8s-sample/BlazorLeaflet/*.csproj ./BlazorLeaflet/
+RUN dotnet restore AdventureWorks.sln
+
+COPY ./adventureworks-k8s-sample/AdventureWorks.App/. ./AdventureWorks.App/
+COPY ./adventureworks-k8s-sample/BlazorLeaflet/. ./BlazorLeaflet/
+
+RUN dotnet publish -c release -o /app --no-restore
+
+FROM $runner
+WORKDIR /app
+COPY --from=build /app ./
+RUN zypper -n in curl
+RUN echo $$'#!/bin/bash -e \n\
+dotnet AdventureWorks.App.dll & \n\
+c=0 \n\
+until curl localhost:5000 >/dev/null ; do \n\
+  ((c++)) && ((c==20)) && ( \n\
+    curl localhost:5000|grep Adventure \n\
+    exit 1 \n\
+  ) \n\
+  sleep 1 \n\
+done \n\
+pkill dotnet \n\
+' > entrypoint.sh && chmod +x entrypoint.sh
+ENTRYPOINT ["/app/entrypoint.sh"]
+""",
+            ),
+            0,
+            """Microsoft.Hosting.Lifetime[0]\n      Now listening on: http://localhost:5000
+\x1b[40m\x1b[32minfo\x1b[39m\x1b[22m\x1b[49m: Microsoft.Hosting.Lifetime[0]
+      Application started. Press Ctrl+C to shut down.
+\x1b[40m\x1b[32minfo\x1b[39m\x1b[22m\x1b[49m: Microsoft.Hosting.Lifetime[0]
+      Hosting environment: Production
+\x1b[40m\x1b[32minfo\x1b[39m\x1b[22m\x1b[49m: Microsoft.Hosting.Lifetime[0]
+      Content root path: /app
+""",
+            marks=pytest.mark.skipif(
+                LOCALHOST.system_info.arch != "x86_64",
+                reason="The dotnet containers are only available for x86_64",
+            ),
         ),
     ],
     indirect=["host_git_clone"],
