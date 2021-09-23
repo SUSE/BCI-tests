055db4fbefb507024eff4e153f16a8312eb451ff
Dan Čermák <dcermak@suse.com>
2021-09-23 15:48:28 +0200

    Add per architecture limits on the base, minimal & micro container size

--- test_minimal.py
+++ test_minimal.py
@@ -1,24 +1,42 @@
+from typing import Dict
+
 import pytest
 from bci_tester.data import MICRO_CONTAINER
 from bci_tester.data import MINIMAL_CONTAINER
+from bci_tester.helpers import LOCALHOST
 
 
 CONTAINER_IMAGES = [MINIMAL_CONTAINER, MICRO_CONTAINER]
 
-MINIMAL_IMAGE_MAX_SIZE = 40 * 1024 * 1024
-MICRO_IMAGE_MAX_SIZE = 25 * 1024 * 1024
+MINIMAL_IMAGE_MAX_SIZE: Dict[str, int] = {
+    "x86_64": 40,
+    "aarch64": 44,
+    "s390x": 40,
+    "ppc64le": 51,
+}
+MICRO_IMAGE_MAX_SIZE: Dict[str, int] = {
+    "x86_64": 25,
+    "aarch64": 26,
+    "s390x": 25,
+    "ppc64le": 31,
+}
 
 
 @pytest.mark.parametrize(
     "container,size",
     [
-        (MICRO_CONTAINER, MICRO_IMAGE_MAX_SIZE),
         (MINIMAL_CONTAINER, MINIMAL_IMAGE_MAX_SIZE),
+        (MICRO_CONTAINER, MICRO_IMAGE_MAX_SIZE),
     ],
     indirect=["container"],
 )
-def test_minimal_image_size(container, size, container_runtime):
-    assert container_runtime.get_image_size(container.image_url_or_id) < size
+def test_minimal_image_size(
+    container, size: Dict[str, int], container_runtime
+):
+    assert (
+        container_runtime.get_image_size(container.image_url_or_id)
+        < size[LOCALHOST.system_info.arch] * 1024 * 1024
+    )
 
 
 def test_fat_packages_absent(auto_container):
