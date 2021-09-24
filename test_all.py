b517a77d66a7768f2e23832ddd7ad831166635d9
Dan Čermák <dcermak@suse.com>
2021-09-24 15:48:06 +0200

    Add regression test for certificates in all containers

--- test_all.py
+++ test_all.py
@@ -1,8 +1,38 @@
+import pytest
 from bci_tester.data import ALL_CONTAINERS
+from bci_tester.data import Container
+from bci_tester.data import EXTRA_BUILD_ARGS
+from bci_tester.data import GO_1_16_BASE_CONTAINER
+from bci_tester.data import MICRO_CONTAINER
+from bci_tester.data import MINIMAL_CONTAINER
+from bci_tester.data import MultiStageBuild
 from bci_tester.data import OS_PRETTY_NAME
 from bci_tester.data import OS_VERSION
 
 CONTAINER_IMAGES = ALL_CONTAINERS
+
+FETCH_SUSE_DOT_COM = """package main
+
+import "net/http"
+
+func main() {
+	_, err := http.Get("https://suse.com/")
+	if err != nil {
+		panic(err)
+	}
+}
+"""
+
+MULTISTAGE_DOCKERFILE = """FROM $builder as builder
+WORKDIR /src
+COPY main.go .
+RUN go build main.go
+
+FROM $runner
+WORKDIR /fetcher/
+COPY --from=builder /src/main .
+CMD ["/fetcher/main"]
+"""
 
 
 def test_os_release(auto_container):
@@ -40,3 +70,39 @@ def test_coreutils_present(auto_container):
 def test_glibc_present(auto_container):
     for binary in ("ldconfig", "ldd"):
         assert auto_container.connection.exists(binary)
+
+
+CERT_ERROR_MARK = pytest.mark.xfail(
+    reason="Certificates are missing in minimal and micro containers"
+)
+TINY_CONTAINERS = [MINIMAL_CONTAINER, MICRO_CONTAINER]
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "runner",
+    [cont for cont in ALL_CONTAINERS if cont not in TINY_CONTAINERS]
+    + [pytest.param(cont, marks=CERT_ERROR_MARK) for cont in TINY_CONTAINERS],
+)
+async def test_certificates_are_present(
+    host, tmp_path, container_runtime, runner: Container
+):
+    multi_stage_build = MultiStageBuild(
+        builder=GO_1_16_BASE_CONTAINER,
+        runner=runner,
+        dockerfile_template=MULTISTAGE_DOCKERFILE,
+    )
+    await multi_stage_build.prepare_build(tmp_path)
+
+    with open(tmp_path / "main.go", "w") as main_go:
+        main_go.write(FETCH_SUSE_DOT_COM)
+
+    cmd = host.run_expect(
+        [0],
+        f"{container_runtime.build_command} {' '.join(EXTRA_BUILD_ARGS)} {tmp_path}",
+    )
+    img_id = container_runtime.get_image_id_from_stdout(cmd.stdout)
+
+    host.run_expect(
+        [0], f"{container_runtime.runner_binary} run --rm {img_id}"
+    )
