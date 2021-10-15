370fef796cda652deeb6e4174d370cac8f9aae32
Dan Čermák <dcermak@suse.com>
2021-10-15 14:39:16 +0200

    Restructure repository tests to cater to the extended package set

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -1,43 +1,117 @@
+"""Tests for the SLE_BCI repository itself: can all packages in the repository
+be installed and ensure that we do not accidentally ship forbidden packages.
+
+"""
+from typing import Callable
+from typing import List
+
 import pytest
 from bci_tester.data import BASE_CONTAINER
 from bci_tester.data import REPOCLOSURE_CONTAINER
 
 
-@pytest.mark.parametrize(
-    "container", [REPOCLOSURE_CONTAINER], indirect=["container"]
-)
-def test_repoclosure(container):
-    # workaround for suse-module-tools requiring '(kmod(sg.ko) if kernel)' which
-    # repoclosure interprets as an issue, because 'kernel' could be present on
-    # the system, but it is not available in the repository
+#: Packages that can be installed, but are reported as having dependency issues
+#: by :command:`dnf repoclosure`.
+#: This is caused by these packages having boolean requires on the kernel, which
+#: is not present in the SLE_BCI repository. We check that these packages can be
+#: installed in :py:func:`test_package_installation`.
+REPOCLOSURE_FALSE_POSITIVES = [
+    "multipath-tools",
+    "patterns-base-fips",
+    "salt-minion",
+    "suse-module-tools",
+]
+
+
+def get_package_list(con) -> List[str]:
     package_list = (
-        container.connection.run_expect(
+        con.run_expect(
             [0],
-            "dnf list --available|grep SLE_BCI|awk '{print $1}'|sed '/suse-module-tools/d'",
+            "dnf list --available|grep SLE_BCI|awk '{print $1}'",
         )
         .stdout.strip()
         .split("\n")
     )
+    assert len(package_list) > 3000
+    return package_list
 
-    # sanity checks
-    assert (
-        len(package_list) > 100
-    ), "just a sanity check that we have a few packages in the list"
-    assert not [
-        pkg for pkg in package_list if "suse-module-tools" in pkg
-    ], "suse-module-tools must not be present in package_list"
 
-    # actual checks
-    assert not [
-        pkg for pkg in package_list if "kernel" in pkg
-    ], "package_list must not contain any kernel packages"
+def package_name_filter_func(
+    packages_to_filter_out: List[str],
+) -> Callable[[str], bool]:
+    """Returns a function to be used with ``filter`` to remove the strings
+    containing any of the strings from `packages_to_filter_out` from an
+    iterable.
 
-    container.connection.run_expect(
+    """
+
+    def f(pkg_name: str) -> bool:
+        for to_filter in packages_to_filter_out:
+            if to_filter in pkg_name:
+                return False
+        return True
+
+    return f
+
+
+@pytest.mark.parametrize(
+    "container_per_test", [REPOCLOSURE_CONTAINER], indirect=True
+)
+def test_repoclosure(container_per_test):
+    """Run :command:`dnf repoclosure` on all packages in the ``SLE_BCI`` repository
+    excluding the packages in :py:const`REPOCLOSURE_FALSE_POSITIVES`.
+
+    """
+    package_list = filter(
+        package_name_filter_func(REPOCLOSURE_FALSE_POSITIVES),
+        get_package_list(container_per_test.connection),
+    )
+
+    container_per_test.connection.run_expect(
         [0], "dnf repoclosure --pkg " + " --pkg ".join(package_list)
     )
 
 
-@pytest.mark.parametrize("pkg", ["git", "curl", "wget", "unzip"])
-@pytest.mark.parametrize("container", [BASE_CONTAINER], indirect=["container"])
-def test_package_installation(container, pkg):
-    container.connection.run_expect([0], f"zypper -n in {pkg}")
+@pytest.mark.parametrize(
+    "container_per_test", [REPOCLOSURE_CONTAINER], indirect=True
+)
+@pytest.mark.xfail(
+    reason="Some kernel related packages are still in the repository"
+)
+def test_forbidden_packages(container_per_test):
+    package_list = get_package_list(container_per_test.connection)
+
+    ALLOWED_PACKAGES = [
+        "system-group-kvm.noarch",
+        "jaxen.noarch",
+        "kernelshark",
+        "librfxencode0",
+        "nfs-kernel-server",
+        "texlive-l3kernel.noarch",
+    ]
+
+    FORBIDDEN_PACKAGE_NAMES = ["kernel", "yast", "kvm", "xen"]
+
+    forbidden_packages = list(
+        filter(
+            package_name_filter_func(ALLOWED_PACKAGES),
+            filter(
+                lambda p: not package_name_filter_func(
+                    FORBIDDEN_PACKAGE_NAMES
+                )(p),
+                package_list,
+            ),
+        )
+    )
+
+    assert (
+        not forbidden_packages
+    ), f"package_list must not contain any forbidden packages, but found {', '.join(forbidden_packages)}"
+
+
+@pytest.mark.parametrize(
+    "pkg", ["git", "curl", "wget", "unzip"] + REPOCLOSURE_FALSE_POSITIVES
+)
+@pytest.mark.parametrize("container_per_test", [BASE_CONTAINER], indirect=True)
+def test_package_installation(container_per_test, pkg):
+    container_per_test.connection.run_expect([0], f"zypper -n in {pkg}")
