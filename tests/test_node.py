a09729a9980674cf5aeabb02a69d10317e80b853
Dan Čermák <dcermak@suse.com>
2021-10-19 12:39:42 +0200

    switch mutating tests to use _per_test fixtures

--- tests/test_node.py
+++ tests/test_node.py
@@ -79,6 +79,30 @@ def test_npm_version(auto_container):
     ],
     indirect=["container_git_clone"],
 )
-def test_popular_npm_repos(auto_container, container_git_clone):
-    cmd = auto_container.connection.run(container_git_clone.test_command)
-    assert cmd.rc == 0
+def test_popular_npm_repos(auto_container_per_test, container_git_clone):
+    """Try to build and run the tests of a few popular npm packages:
+
+    .. list-table::
+       :header-rows: 1
+
+       * - package
+         - build command
+       * - `Lodash <https://github.com/lodash/lodash.git>`_
+         - :command:`npm ci && npm test`
+       * - `chalk <https://github.com/chalk/chalk.git>`_
+         - :command:`npm install && npm test`
+       * - `Commander.js <https://github.com/tj/commander.js.git>`_
+         - :command:`npm ci && npm test && npm run lint`
+       * - `Express <https://github.com/expressjs/express.git>`_
+         - :command:`npm config set shrinkwrap false && npm rm --silent --save-dev connect-redis && npm run test-ci && npm run lint`
+       * - `moment <https://github.com/moment/moment>`_
+         - :command:`npm install -g grunt-cli && npm install && grunt test`
+       * - `prop-types <https://github.com/facebook/prop-types>`_
+         - :command:`npm -g install yarn && yarn --frozen-lockfile && yarn run build && yarn run pretest && yarn run tests-only`
+       * - `node-fs-extra <https://github.com/jprichardson/node-fs-extra>`_
+         - :command:`npm install && npm run unit`
+
+    """
+    auto_container_per_test.connection.run_expect(
+        [0], container_git_clone.test_command
+    )
