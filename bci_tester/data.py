SUSE/BCI-tests

commit 967db993afe3e1349538e9fde8610324e319406e

Only add marks for containers available for this os version

bci_tester/data.py
@@ -270,7 +270,7 @@ def create_BCI(
             of the :py:class:`~pytest_container.DerivedContainer`
     """
     build_tag_base = build_tag.rpartition("/")[2]
-    marks = [pytest.mark.__getattr__(build_tag_base.replace(":", "_"))]
+    marks = []
     if extra_marks:
         for m in extra_marks:
             marks.append(m)
@@ -293,14 +293,17 @@ def create_BCI(
                     raise ValueError(
                         f"Invalid os version for a language or application stack container: {ver}"
                     )
-            marks.append(create_container_version_mark(available_versions))
         else:
-            marks.append(
-                create_container_version_mark(_DEFAULT_NONBASE_OS_VERSIONS)
-            )
+            available_versions = list(_DEFAULT_NONBASE_OS_VERSIONS)
 
-    elif available_versions is not None:
+    if available_versions:
         marks.append(create_container_version_mark(available_versions))
+
+    # only try to grab the mark from the build tag for containers that are
+    # available for this os version, otherwise we get bogus errors for missing
+    # marks
+    if OS_VERSION in (available_versions or []):
+        marks.append(pytest.mark.__getattr__(build_tag_base.replace(":", "_")))
 
     if OS_VERSION == "tumbleweed":
         if bci_type == ImageType.APPLICATION:

commit 7880f79559fc99cc662258b39bbb85d9c8c06df6

Create the marks for default_repos for Base containers again
This is a regression from 967db993afe3e1349538e9fde8610324e319406e
which broke BCI repository pipeline

bci_tester/data.py
@@ -302,7 +302,9 @@ def create_BCI(
     # only try to grab the mark from the build tag for containers that are
     # available for this os version, otherwise we get bogus errors for missing
     # marks
-    if OS_VERSION in (available_versions or []):
+    if OS_VERSION in (
+        available_versions or list(_DEFAULT_NONBASE_OS_VERSIONS)
+    ):
         marks.append(pytest.mark.__getattr__(build_tag_base.replace(":", "_")))
 
     if OS_VERSION == "tumbleweed":