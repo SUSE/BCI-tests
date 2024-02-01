e719217ac5ed690ce8d67f2b5f809c9b43349bad
Dirk Müller <dirk@dmllr.de>
2024-02-01 14:50:48 +0100

    Typo fix

--- tests/test_all.py
+++ tests/test_all.py
@@ -226,7 +226,7 @@ def test_glibc_present(auto_container):
 )
 @pytest.mark.parametrize("container", CONTAINERS_WITH_ZYPPER, indirect=True)
 def test_no_downgrade_on_install(container: ContainerData) -> None:
-    """Check that we can install any additional pacakage in the container.
+    """Check that we can install any additional package in the container.
 
     Check that installing any additional package would not cause a downgrade
     of any package already installed in the container as that would throw
