3639a2e302761bdc156e62b58e017af694e9d192
Witek Bedyk <witold.bedyk@suse.com>
2024-06-13 10:46:42 +0200

    Fix formatting errors

--- tests/test_prometheus.py
+++ tests/test_prometheus.py
@@ -9,30 +9,22 @@ from bci_tester.data import PROMETHEUS_CONTAINERS
 
 @pytest.mark.parametrize("container", PROMETHEUS_CONTAINERS, indirect=True)
 def test_prometheus_ready(container: ContainerData) -> None:
-    """Simple smoke test verifying that Prometheus is ready.
-
-    """
+    """Simple smoke test verifying that Prometheus is ready."""
 
     port = container.forwarded_ports[0].host_port
     resp = requests.get(f"http://localhost:{port}/-/ready", timeout=2)
     baseurl = container.container.baseurl
     assert baseurl
-    ver = baseurl.rpartition(":")[2]
     assert resp.status_code == 200
     assert resp.text == "Prometheus Server is Ready.\n"
 
 
 @pytest.mark.parametrize("container", PROMETHEUS_CONTAINERS, indirect=True)
 def test_prometheus_healthy(container: ContainerData) -> None:
-    """Simple smoke test verifying that Prometheus is healthy.
-
-    """
+    """Simple smoke test verifying that Prometheus is healthy."""
 
     port = container.forwarded_ports[0].host_port
     resp = requests.get(f"http://localhost:{port}/-/healthy", timeout=2)
     baseurl = container.container.baseurl
-    assert baseurl
-    ver = baseurl.rpartition(":")[2]
     assert resp.status_code == 200
     assert resp.text == "Prometheus Server is Healthy.\n"
-
