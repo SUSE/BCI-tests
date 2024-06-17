96a3e495385723331e04c0ebc7ed7f3d4b7e90af
Witek Bedyk <witold.bedyk@suse.com>
2024-06-17 17:18:30 +0200

    Rename containers lists

--- tests/test_prometheus.py
+++ tests/test_prometheus.py
@@ -9,25 +9,27 @@ from bci_tester.data import ALERTMANAGER_CONTAINERS
 from bci_tester.data import BLACKBOX_CONTAINERS
 
 
-ready_containers = PROMETHEUS_CONTAINERS + ALERTMANAGER_CONTAINERS
-healthy_containers = (
+PROMETHEUS_STACK_CONTAINERS = PROMETHEUS_CONTAINERS + ALERTMANAGER_CONTAINERS
+PROMETHEUS_AND_BLACKBOX_CONTAINERS = (
     PROMETHEUS_CONTAINERS + ALERTMANAGER_CONTAINERS + BLACKBOX_CONTAINERS
 )
 
 
-@pytest.mark.parametrize("container", ready_containers, indirect=True)
+@pytest.mark.parametrize(
+    "container", PROMETHEUS_STACK_CONTAINERS, indirect=True
+)
 def test_prometheus_ready(container: ContainerData) -> None:
     """Simple smoke test verifying that Prometheus is ready."""
 
     port = container.forwarded_ports[0].host_port
     resp = requests.get(f"http://localhost:{port}/-/ready", timeout=2)
-    baseurl = container.container.baseurl
-    assert baseurl
     assert resp.status_code == 200
     assert resp.text in ["Prometheus Server is Ready.\n", "OK"]
 
 
-@pytest.mark.parametrize("container", healthy_containers, indirect=True)
+@pytest.mark.parametrize(
+    "container", PROMETHEUS_AND_BLACKBOX_CONTAINERS, indirect=True
+)
 def test_prometheus_healthy(container: ContainerData) -> None:
     """Simple smoke test verifying that Prometheus is healthy."""
 
