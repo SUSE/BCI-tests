12fef37140efaac5c8d660b5cbaafb1adee959cc
Madhankumar Chellamuthu <rcmadhankumar@gmail.com>
2024-08-20 19:45:47 +0530

    version test

--- tests/test_fips.py
+++ tests/test_fips.py
@@ -204,13 +204,15 @@ def test_gnutls_binary(container_per_test: ContainerData) -> None:
 
     """
 
-    container_per_test.connection.check_output(
+    rr = container_per_test.connection.check_output(
         "ls /usr/lib64/libgnutl* && "
         "rpm -q libgnutls-devel && "
         "zypper --gpg-auto-import-keys -n ref && zypper -n in gcc gnutls gnutls-devel && zypper -n clean && "
         "gcc -Og -g3 fips-test-gnutls.c -Wall -Wextra -Wpedantic -lgnutls -o fips-test-gnutls && "
         "mv fips-test-gnutls /bin/fips-test-gnutls"
     )
+
+    assert "failureeee" in rr
 
     expected_fips_gnutls_digests = {
         "md5": "bcfe53d9a81f2d0382a280d017796084e59ff97941044f85df5297e1c302d260",
