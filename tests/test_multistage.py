54f59cffc00bf43a00e58159950fdd4c1bc69a85
Dan Čermák <dcermak@suse.com>
2024-08-27 10:24:00 +0200

    WIP: refactor tests using MultiStageBuild to use MultiStageContainer & container_image

--- tests/test_multistage.py
+++ tests/test_multistage.py
@@ -1,15 +1,13 @@
 """Integration tests via multistage container builds."""
 
+from typing import Optional
+
 import pytest
-from _pytest.config import Config
-from pytest_container import GitRepositoryBuild
-from pytest_container import MultiStageBuild
-from pytest_container import get_extra_build_args
-from pytest_container import get_extra_run_args
+from pytest_container import container_and_marks_from_pytest_param
+from pytest_container.container import ContainerImageData
+from pytest_container.container import MultiStageContainer
 from pytest_container.runtime import LOCALHOST
 
-from bci_tester.data import DOTNET_ASPNET_8_0_CONTAINER
-from bci_tester.data import DOTNET_SDK_8_0_CONTAINER
 from bci_tester.data import GOLANG_CONTAINERS
 from bci_tester.data import OPENJDK_11_CONTAINER
 from bci_tester.data import OPENJDK_21_CONTAINER
@@ -17,15 +15,24 @@ from bci_tester.data import OPENJDK_DEVEL_11_CONTAINER
 from bci_tester.data import OPENJDK_DEVEL_21_CONTAINER
 from bci_tester.data import OS_VERSION
 
+
+def _clone_cmd(repo_url: str, tag: Optional[str] = None) -> str:
+    res = "git clone --depth 1 "
+    if tag:
+        res += f"--branch {tag} "
+    return res + repo_url
+
+
 #: maven version that is being build in the multistage test build
-MAVEN_VERSION = "3.9.6"
+MAVEN_VERSION = "3.9.9"
 
 #: Dockerfile template to build `amidst
 #: <https://github.com/toolbox4minecraft/amidst>`_
-AMIDST_DOCKERFILE = """FROM $builder as builder
+AMIDST_DOCKERFILE = f"""FROM $builder as builder
+RUN {_clone_cmd('https://github.com/toolbox4minecraft/amidst', 'v4.7')} /amidst
 WORKDIR /amidst
-COPY ./amidst .
 RUN mvn package -DskipTests=True
+
 FROM $runner
 WORKDIR /amidst/
 COPY --from=builder /amidst/target .
@@ -35,10 +42,11 @@ CMD ["java", "-jar", "amidst-v4-7.jar"]
 #: template of a Dockerfile to build maven in the OpenJDK devel container and
 #: copy it into the OpenJDK base container
 MAVEN_BUILD_DOCKERFILE = f"""FROM $builder as builder
+RUN {_clone_cmd('https://github.com/apache/maven', 'maven-' + MAVEN_VERSION)} /maven
 WORKDIR /maven
-COPY ./maven .
 RUN mvn package && zypper -n in unzip && \
     unzip /maven/apache-maven/target/apache-maven-{MAVEN_VERSION}-bin.zip
+
 FROM $runner
 WORKDIR /maven/
 COPY --from=builder /maven/apache-maven-{MAVEN_VERSION}/ .
@@ -47,9 +55,9 @@ CMD ["/maven/bin/mvn"]
 
 #: Dockerfile to build pdftk in the openjdk devel container and transfer it into
 #: the openjdk container.
-PDFTK_BUILD_DOCKERFILE = """FROM $builder as builder
+PDFTK_BUILD_DOCKERFILE = f"""FROM $builder as builder
+RUN {_clone_cmd('https://gitlab.com/pdftk-java/pdftk.git', 'v3.3.3')} /pdftk
 WORKDIR /pdftk
-COPY ./pdftk .
 RUN zypper -n in apache-ant apache-ivy && ant test-resolve && ant compile && ant jar
 
 FROM $runner
@@ -61,10 +69,10 @@ CMD ["java", "-jar", "pdftk.jar"]
 
 #: dockerfile template to build k3sup in the go
 #: container and transfer it into a scratch container
-K3SUP_DOCKERFILE = """FROM $builder as builder
+K3SUP_DOCKERFILE = f"""FROM $builder as builder
+RUN {_clone_cmd('https://github.com/alexellis/k3sup', '0.13.7')} /k3sup
 WORKDIR /k3sup
-COPY ./k3sup .
-RUN zypper -n in make && echo > ./hack/hashgen.sh && make all
+RUN echo > ./hack/hashgen.sh && make all
 
 FROM $runner
 WORKDIR /k3sup
@@ -72,43 +80,6 @@ COPY --from=builder /k3sup/bin/k3sup .
 CMD ["/k3sup/k3sup"]
 """
 
-#: modified version of upstream's `Dockerfile
-#: <https://github.com/phillipsj/adventureworks-k8s-sample/blob/main/Dockerfile>`_:
-#:
-#: - the ``entrypoint.sh`` is custom, so that the application terminates
-#: - the docker build is not run from the repos top level dir, but one directory
-#:   "above"
-DOTNET_K8S_SAMPLE_DOCKERFILE = r"""FROM $builder AS build
-WORKDIR /src
-
-COPY ./adventureworks-k8s-sample/AdventureWorks.sln AdventureWorks.sln
-COPY ./adventureworks-k8s-sample/AdventureWorks.App/*.csproj ./AdventureWorks.App/
-COPY ./adventureworks-k8s-sample/BlazorLeaflet/*.csproj ./BlazorLeaflet/
-RUN dotnet restore AdventureWorks.sln
-
-COPY ./adventureworks-k8s-sample/AdventureWorks.App/. ./AdventureWorks.App/
-COPY ./adventureworks-k8s-sample/BlazorLeaflet/. ./BlazorLeaflet/
-
-RUN dotnet publish -c release -o /app --no-restore
-
-FROM $runner
-WORKDIR /app
-COPY --from=build /app ./
-RUN zypper -n in curl
-RUN echo $$'#!/bin/bash -e \n\
-dotnet AdventureWorks.App.dll & \n\
-c=0 \n\
-until curl localhost:5000 >/dev/null ; do \n\
-  ((c++)) && ((c==20)) && ( \n\
-    curl localhost:5000|grep Adventure \n\
-    exit 1 \n\
-  ) \n\
-  sleep 1 \n\
-done \n\
-pkill dotnet \n\
-' > entrypoint.sh && chmod +x entrypoint.sh
-ENTRYPOINT ["/app/entrypoint.sh"]
-"""
 
 OPENJDK_DEVEL_CONTAINER = OPENJDK_DEVEL_11_CONTAINER
 if OS_VERSION in ("15.6",):
@@ -118,118 +89,79 @@ OPENJDK_CONTAINER = OPENJDK_11_CONTAINER
 if OS_VERSION in ("15.6",):
     OPENJDK_CONTAINER = OPENJDK_21_CONTAINER
 
+_jdk_devel_ctr, _jdk_devel_marks = container_and_marks_from_pytest_param(
+    OPENJDK_DEVEL_CONTAINER
+)
+_jdk_ctr, _jdk_marks = container_and_marks_from_pytest_param(OPENJDK_CONTAINER)
+
 
 @pytest.mark.parametrize(
-    "host_git_clone,multi_stage_build,retval,cmd_stdout",
+    "container_image, retval, expected_stdout",
     [
-        (
-            GitRepositoryBuild(
-                repository_url="https://github.com/toolbox4minecraft/amidst",
-                repository_tag="v4.7",
-            ),
-            MultiStageBuild(
+        pytest.param(
+            MultiStageContainer(
                 containers={
-                    "builder": OPENJDK_DEVEL_CONTAINER,
-                    "runner": OPENJDK_CONTAINER,
+                    "builder": _jdk_devel_ctr,
+                    "runner": _jdk_ctr,
                 },
-                containerfile_template=AMIDST_DOCKERFILE,
+                containerfile=AMIDST_DOCKERFILE,
             ),
             0,
             "[info] Amidst v4.7",
+            marks=_jdk_marks + _jdk_devel_marks,
         ),
-        (
-            GitRepositoryBuild(
-                repository_url="https://github.com/apache/maven",
-                repository_tag=f"maven-{MAVEN_VERSION}",
-            ),
-            MultiStageBuild(
-                containers={
-                    "builder": OPENJDK_DEVEL_CONTAINER,
-                    "runner": OPENJDK_DEVEL_CONTAINER,
-                },
-                containerfile_template=MAVEN_BUILD_DOCKERFILE,
-            ),
-            1,
-            "[ERROR] No goals have been specified for this build.",
-        ),
-        (
-            GitRepositoryBuild(
-                repository_tag="v3.3.3",
-                repository_url="https://gitlab.com/pdftk-java/pdftk.git",
-            ),
-            MultiStageBuild(
-                containers={
-                    "builder": OPENJDK_DEVEL_CONTAINER,
-                    "runner": OPENJDK_CONTAINER,
-                },
-                containerfile_template=PDFTK_BUILD_DOCKERFILE,
+        pytest.param(
+            MultiStageContainer(
+                containers={"builder": _jdk_devel_ctr, "runner": _jdk_ctr},
+                containerfile=PDFTK_BUILD_DOCKERFILE,
             ),
             0,
             """SYNOPSIS
-       pdftk <input PDF files | - | PROMPT>
-        """,
+       pdftk <input PDF files | - | PROMPT>""",
+            marks=_jdk_marks + _jdk_devel_marks,
         ),
         pytest.param(
-            GitRepositoryBuild(
-                repository_tag="0.12.4",
-                repository_url="https://github.com/alexellis/k3sup",
-            ),
-            MultiStageBuild(
+            MultiStageContainer(
                 containers={
-                    "builder": GOLANG_CONTAINERS[-1],
+                    "builder": _jdk_devel_ctr,
+                    "runner": _jdk_devel_ctr,
+                },
+                containerfile=MAVEN_BUILD_DOCKERFILE,
+            ),
+            1,
+            "No goals have been specified for this build.",
+            marks=_jdk_devel_marks,
+        ),
+    ]
+    + [
+        pytest.param(
+            MultiStageContainer(
+                containers={
+                    "builder": container_and_marks_from_pytest_param(param)[0],
                     "runner": "scratch",
                 },
-                containerfile_template=K3SUP_DOCKERFILE,
+                containerfile=K3SUP_DOCKERFILE,
             ),
             0,
             'Use "k3sup [command] --help" for more information about a command.',
-            marks=pytest.mark.xfail(
-                condition=LOCALHOST.system_info.arch != "x86_64",
-                reason="Currently broken on arch != x86_64, see https://github.com/alexellis/k3sup/pull/345",
-            ),
-        ),
-        pytest.param(
-            GitRepositoryBuild(
-                repository_url="https://github.com/phillipsj/adventureworks-k8s-sample.git"
-            ),
-            MultiStageBuild(
-                containers={
-                    "builder": DOTNET_SDK_8_0_CONTAINER,
-                    "runner": DOTNET_ASPNET_8_0_CONTAINER,
-                },
-                containerfile_template=DOTNET_K8S_SAMPLE_DOCKERFILE,
-            ),
-            0,
-            """Microsoft.Hosting.Lifetime[0]\n      Now listening on: http://localhost:5000
-\x1b[40m\x1b[32minfo\x1b[39m\x1b[22m\x1b[49m: Microsoft.Hosting.Lifetime[0]
-      Application started. Press Ctrl+C to shut down.
-\x1b[40m\x1b[32minfo\x1b[39m\x1b[22m\x1b[49m: Microsoft.Hosting.Lifetime[0]
-      Hosting environment: Production
-\x1b[40m\x1b[32minfo\x1b[39m\x1b[22m\x1b[49m: Microsoft.Hosting.Lifetime[0]
-      Content root path: /app
-""",
             marks=[
-                pytest.mark.skipif(
-                    LOCALHOST.system_info.arch != "x86_64",
-                    reason="The dotnet containers are only available for x86_64",
-                ),
-                pytest.mark.skip(
-                    reason="This relies on .Net 5, which reached Eol, see: https://github.com/phillipsj/adventureworks-k8s-sample/issues/4"
-                ),
-            ],
-        ),
+                pytest.mark.xfail(
+                    condition=LOCALHOST.system_info.arch != "x86_64",
+                    reason="Currently broken on arch != x86_64, see https://github.com/alexellis/k3sup/pull/345",
+                )
+            ]
+            + param.marks,
+        )
+        for param in GOLANG_CONTAINERS
     ],
-    indirect=["host_git_clone"],
+    indirect=["container_image"],
 )
-def test_dockerfile_build(
-    host,
-    container_runtime,
-    host_git_clone,
-    multi_stage_build: MultiStageBuild,
+def test_multistage_build(
+    container_image: ContainerImageData,
     retval: int,
-    cmd_stdout: str,
-    pytestconfig: Config,
-):
+    expected_stdout: str,
+    host,
+) -> None:
     """Integration test of multistage container builds. We fetch a project
     (optionally checking out a specific tag), run a two stage build using a
     dockerfile template where we substitute the ``$runner`` and ``$builder``
@@ -259,7 +191,7 @@ def test_dockerfile_build(
          - OpenJDK devel
          - OpenJDK
          - ``1``
-         - ``[ERROR] No goals have been specified for this build.``
+         - ``No goals have been specified for this build.``
 
        * - `<https://gitlab.com/pdftk-java/pdftk.git>`_ at ``v3.3.1``
          - :py:const:`PDFTK_BUILD_DOCKERFILE`
@@ -275,26 +207,8 @@ def test_dockerfile_build(
          - ``0``
          - ``Use "k3sup [command] --help" for more information about a command.``
 
-       * - `<https://github.com/phillipsj/adventureworks-k8s-sample.git>`_
-         - :py:const:`DOTNET_K8S_SAMPLE_DOCKERFILE`
-         - .Net 5.0
-         - ASP.Net 5.0
-         - ``0``
-         - `omitted`
     """
-    tmp_path, _ = host_git_clone
 
-    img_id = multi_stage_build.build(
-        tmp_path,
-        pytestconfig,
-        container_runtime,
-        extra_build_args=get_extra_build_args(pytestconfig),
-    )
-
-    assert (
-        cmd_stdout
-        in host.run_expect(
-            [retval],
-            f"{container_runtime.runner_binary} run --rm {' '.join(get_extra_run_args(pytestconfig))}{img_id}",
-        ).stdout
-    )
+    assert expected_stdout in host.run_expect(
+        [retval], container_image.run_command
+    ).stdout.replace("\r", "")
