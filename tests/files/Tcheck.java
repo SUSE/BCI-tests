f9b4f16e8c7dee8d4bf608a21e137ab7c73b9425
Pablo Herranz Ramírez <pablo.herranz@suse.com>
2024-12-04 11:24:17 +0100

    Added test for openJDK in FIPS mode. Fixes issue #614

---
+++ tests/files/Tcheck.java
@@ -0,0 +1,17 @@
+import javax.net.ssl.SSLContext;
+import java.security.NoSuchAlgorithmException;
+import java.security.Provider;
+import java.security.Security;
+
+public class Tcheck {
+
+	public static void main(String[] args) {
+		int i = 1;
+		System.out.println("Supported Security Providers:");
+		Provider [] providers = Security.getProviders();
+
+		for (Provider provider: providers) {
+			System.out.println(" " + i++ + ". " + provider.getInfo());
+		}
+	}
+}
