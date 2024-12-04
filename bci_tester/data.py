f4cf8b7c0053f6819b825ad32b0f6b3c8b362485
Dirk Müller <dirk@dmllr.de>
2024-12-04 16:53:29 +0100

    Add node 22 testing for SP7

--- bci_tester/data.py
+++ bci_tester/data.py
@@ -534,7 +534,7 @@ NODEJS_20_CONTAINER = create_BCI(
 
 NODEJS_22_CONTAINER = create_BCI(
     build_tag="bci/nodejs:22",
-    available_versions=["tumbleweed"],
+    available_versions=["tumbleweed", "15.7"],
 )
 
 NODEJS_CONTAINERS = [
