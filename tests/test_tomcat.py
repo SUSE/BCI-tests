fb76c86ca1d564af5dd533b6ab88e766857cce8c
Dan Čermák <dcermak@suse.com>
2024-12-11 15:52:19 +0100

    [tomcat] stop needing curl for the tests

--- tests/test_tomcat.py
+++ tests/test_tomcat.py
@@ -1,32 +1,14 @@
 """Tests for the tomcat containers."""
 
+from pathlib import Path
+
 import pytest
 import requests
 import tenacity
-from pytest_container import DerivedContainer
-from pytest_container import container_and_marks_from_pytest_param
+from pytest_container import OciRuntimeBase
 from pytest_container.container import ContainerData
-from pytest_container.container import ImageFormat
 
 from bci_tester.data import TOMCAT_CONTAINERS
-
-TOMCAT_WITH_SAMPLE = []
-
-for tomcat_ctr in TOMCAT_CONTAINERS:
-    ctr, marks = container_and_marks_from_pytest_param(tomcat_ctr)
-    TOMCAT_WITH_SAMPLE.append(
-        pytest.param(
-            DerivedContainer(
-                base=ctr,
-                forwarded_ports=ctr.forwarded_ports,
-                containerfile="""RUN cd $CATALINA_HOME/webapps; curl -sfO https://tomcat.apache.org/tomcat-10.1-doc/appdev/sample/sample.war
-HEALTHCHECK --interval=5s --timeout=5s --retries=5 CMD ["/usr/bin/curl", "-sf", "http://localhost:8080/sample"]
-""",
-                image_format=ImageFormat.DOCKER,
-            ),
-            marks=marks,
-        )
-    )
 
 
 @pytest.mark.parametrize("container", TOMCAT_CONTAINERS, indirect=True)
@@ -55,21 +37,46 @@ def test_tomcat_launches(container: ContainerData) -> None:
 
 
 @pytest.mark.parametrize(
-    "container_per_test", TOMCAT_WITH_SAMPLE, indirect=True
+    "container_per_test", TOMCAT_CONTAINERS, indirect=True
 )
-def test_tomcat_launches_sample_app(container_per_test: ContainerData) -> None:
-    """Launches a container derived from the tomcat containers that includes the
-    `Tomcat sample application
-    <https://tomcat.apache.org/tomcat-10.1-doc/appdev/sample/>`_ in the
-    :file:`$CATALINA_HOME/webapps` directory. Tomcat should serve this sample
-    app automatically via the `GET /sample` route. We test that Tomcat responds
-    to `GET /sample` with a response containing `Sample "Hello, World"
-    Application`.
+def test_tomcat_launches_sample_app(
+    container_per_test: ContainerData,
+    host,
+    tmp_path: Path,
+    container_runtime: OciRuntimeBase,
+) -> None:
+    """Launches a tomcat container. The test downloads the `Tomcat sample
+    application <https://tomcat.apache.org/tomcat-10.1-doc/appdev/sample/>`_ and
+    copies it into the :file:`$CATALINA_HOME/webapps` directory, which then
+    tomcat should serve automatically via the `GET /sample` route. We test that
+    Tomcat responds to `GET /sample` with a response containing ``Sample "Hello,
+    World" Application``.
 
     """
-    resp = requests.get(
-        f"http://localhost:{container_per_test.forwarded_ports[0].host_port}/sample",
-        timeout=2,
+
+    sample_req = requests.get(
+        "https://tomcat.apache.org/tomcat-10.1-doc/appdev/sample/sample.war"
     )
+    sample_req.raise_for_status()
+
+    sample_war_dest = str(tmp_path / "sample.war")
+    with open(sample_war_dest, "wb") as sample_war:
+        for chunk in sample_req.iter_content():
+            sample_war.write(chunk)
+
+    host.check_output(
+        f"{container_runtime.runner_binary} cp {sample_war_dest} {container_per_test.container_id}:/srv/tomcat/webapps/"
+    )
+
+    @tenacity.retry(
+        stop=tenacity.stop_after_attempt(5), wait=tenacity.wait_exponential()
+    )
+    def _fetch_tomcat_sample() -> requests.Response:
+        return requests.get(
+            f"http://localhost:{container_per_test.forwarded_ports[0].host_port}/sample",
+            timeout=2,
+        )
+
+    resp = _fetch_tomcat_sample()
     assert resp.status_code == 200
     assert 'Sample "Hello, World" Application' in resp.text
