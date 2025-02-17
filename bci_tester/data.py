ecc5e039f41e5e725b914e8b15308440e30725e1
Dirk Müller <dirk@dmllr.de>
2025-02-17 14:47:55 +0100

    Add Ruby 3.4 testing

--- bci_tester/data.py
+++ bci_tester/data.py
@@ -617,11 +617,15 @@ RUBY_25_CONTAINER = create_BCI(
     build_tag="bci/ruby:2.5", available_versions=["15.6"]
 )
 
+RUBY_34_CONTAINER = create_BCI(
+    build_tag="bci/ruby:3.4", available_versions=["15.7"]
+)
+
 RUBY_LATEST_CONTAINER = create_BCI(
     build_tag="bci/ruby:latest", available_versions=["tumbleweed"]
 )
 
-RUBY_CONTAINERS = [RUBY_25_CONTAINER, RUBY_LATEST_CONTAINER]
+RUBY_CONTAINERS = [RUBY_25_CONTAINER, RUBY_34_CONTAINER, RUBY_LATEST_CONTAINER]
 
 _DOTNET_SKIP_ARCH_MARK = pytest.mark.skipif(
     LOCALHOST.system_info.arch != "x86_64",
