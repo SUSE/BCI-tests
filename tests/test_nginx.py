68823172f781c16f9fa722c0a9d7f12e0b8ad287
rcmadhankumar <madhankumar.chellamuthu@suse.com>
2025-10-13 11:28:38 +0530

    Added tests for running nginx and valkey as a non root user

--- tests/test_nginx.py
+++ tests/test_nginx.py
@@ -1,6 +1,13 @@
 """This module contains the tests for the nginx container, the image with nginx pre-installed."""
 
+from typing import List
+
+import pytest
 import requests
+from _pytest.mark import ParameterSet
+from pytest_container.container import ContainerData
+from pytest_container.container import DerivedContainer
+from pytest_container.container import container_and_marks_from_pytest_param
 from tenacity import retry
 from tenacity import stop_after_attempt
 from tenacity import wait_exponential
@@ -10,9 +17,38 @@ from bci_tester.data import NGINX_CONTAINER
 CONTAINER_IMAGES = (NGINX_CONTAINER,)
 
 
-def test_nginx_welcome_page(auto_container):
+def _generate_test_matrix() -> List[ParameterSet]:
+    params = []
+    for ng_cont_param in CONTAINER_IMAGES:
+        ng_cont = container_and_marks_from_pytest_param(ng_cont_param)[0]
+        marks = ng_cont_param.marks
+        ports = ng_cont.forwarded_ports
+        params.append(pytest.param(ng_cont, marks=marks))
+
+        params.append(
+            pytest.param(
+                DerivedContainer(
+                    base=ng_cont,
+                    forwarded_ports=ports,
+                    extra_launch_args=(["--user", "nginx"]),
+                ),
+                marks=marks,
+            )
+        )
+
+    return params
+
+
+@pytest.mark.parametrize(
+    "container_per_test",
+    _generate_test_matrix(),
+    indirect=["container_per_test"],
+)
+def test_nginx_welcome_page(
+    container_per_test: ContainerData,
+):
     """test that the default welcome page is served by the container."""
-    host_port = auto_container.forwarded_ports[0].host_port
+    host_port = container_per_test.forwarded_ports[0].host_port
 
     # Retry 5 times with exponential backoff delay
     @retry(
--- tests/test_valkey.py
+++ tests/test_valkey.py
@@ -1,8 +1,13 @@
 """This module contains the tests for the valkey container."""
 
 import socket
+from typing import List
 
+import pytest
+from _pytest.mark import ParameterSet
 from pytest_container.container import ContainerData
+from pytest_container.container import DerivedContainer
+from pytest_container.container import container_and_marks_from_pytest_param
 from tenacity import retry
 from tenacity import stop_after_attempt
 from tenacity import wait_exponential
@@ -12,9 +17,39 @@ from bci_tester.data import VALKEY_CONTAINERS
 CONTAINER_IMAGES = VALKEY_CONTAINERS
 
 
-def test_valkey_ping(auto_container: ContainerData):
+def _generate_test_matrix() -> List[ParameterSet]:
+    params = []
+
+    for vk_cont_param in VALKEY_CONTAINERS:
+        vk_cont = container_and_marks_from_pytest_param(vk_cont_param)[0]
+        marks = vk_cont_param.marks
+        ports = vk_cont.forwarded_ports
+        params.append(pytest.param(vk_cont, marks=marks))
+
+        params.append(
+            pytest.param(
+                DerivedContainer(
+                    base=vk_cont,
+                    forwarded_ports=ports,
+                    extra_launch_args=(["--user", "valkey"]),
+                ),
+                marks=marks,
+            )
+        )
+
+    return params
+
+
+@pytest.mark.parametrize(
+    "container_per_test",
+    _generate_test_matrix(),
+    indirect=["container_per_test"],
+)
+def test_valkey_ping(
+    container_per_test: ContainerData,
+):
     """Test that we can reach valkey port successfully."""
-    host_port = auto_container.forwarded_ports[0].host_port
+    host_port = container_per_test.forwarded_ports[0].host_port
 
     # Retry 5 times with exponential backoff delay
     @retry(
