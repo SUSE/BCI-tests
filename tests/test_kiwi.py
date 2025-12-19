f35868597d06fad0ecce1eb570cc16cff21113df
Alexandre Vicenzi <alexandre.vicenzi@suse.com>
2025-12-19 15:15:12 +0100

    Pin Kiwi test to a version
    A recent change in Kiwi broke the tests, and a proper fix in unclear.
    Using a known working Kiwi version solves the problem.
    
    See bsc#1255398 for more details.

--- tests/test_kiwi.py
+++ tests/test_kiwi.py
@@ -19,7 +19,7 @@ CONTAINER_IMAGES = KIWI_CONTAINERS
 KIWI_CONTAINER_EXTENDED = []
 
 CONTAINERFILE_KIWI_EXTENDED = """
-RUN curl -Lsf -o - https://github.com/OSInside/kiwi/archive/refs/heads/master.tar.gz | tar --no-same-permissions --no-same-owner -xzf - | true
+RUN curl -Lsf -o - https://github.com/OSInside/kiwi/archive/refs/tags/v10.2.34.tar.gz | tar --no-same-permissions --no-same-owner -xzf - | true
 """
 
 for kiwi_ctr in KIWI_CONTAINERS:
@@ -76,9 +76,11 @@ def test_kiwi_create_image(
         in container_per_test.connection.check_output("kiwi-ng --version")
     )
 
-    assert container_per_test.connection.file("kiwi-main/build-tests").exists
+    assert container_per_test.connection.file(
+        "kiwi-10.2.34/build-tests"
+    ).exists
 
-    kiwi_cmd = "kiwi-ng system build --description kiwi-main/build-tests/x86/leap/test-image-disk --set-repo obs://openSUSE:Leap:15.6/standard --target-dir /tmp/myimage"
+    kiwi_cmd = "kiwi-ng system build --description kiwi-10.2.34/build-tests/x86/leap/test-image-disk --set-repo obs://openSUSE:Leap:15.6/standard --target-dir /tmp/myimage"
     res = container_per_test.connection.run_expect([0, 1], kiwi_cmd)
 
     if res.rc == 1:
