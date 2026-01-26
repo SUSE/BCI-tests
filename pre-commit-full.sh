f4e74592f910742fc97b372d80e38cb7258ffd1d
Dirk Müller <dirk@dmllr.de>
2026-01-26 14:04:32 +0100

    Drop 15.3, add 16.1 from a markers perspective

--- pre-commit-full.sh
+++ pre-commit-full.sh
@@ -2,7 +2,7 @@
 
 tox -e format -- --check
 
-for OS_VERSION in "15.4" "15.5" "15.6" "15.6-spr" "15.7" "15.7-spr" "16.0" "tumbleweed"; do
+for OS_VERSION in "15.4" "15.5" "15.6" "15.6-spr" "15.7" "15.7-spr" "16.0" "16.1" "tumbleweed"; do
     export OS_VERSION
     tox -e check_marks
 done
