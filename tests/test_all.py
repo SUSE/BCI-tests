0536a32673162dc8c3cc24fdbb64426180cdcb4a
Dirk Müller <dirk@dmllr.de>
2026-02-26 10:38:20 +0100

    Disable downgrade-test for 16.1

--- tests/test_all.py
+++ tests/test_all.py
@@ -282,7 +282,11 @@ for param in CONTAINERS_WITH_ZYPPER_AS_ROOT:
 
 
 @pytest.mark.skipif(
-    OS_VERSION not in ALLOWED_BCI_REPO_OS_VERSIONS or OS_VERSION == "15.6",
+    OS_VERSION.startswith("16.1"),
+    reason="16.1 is not having regular bci-repo publishes",
+)
+@pytest.mark.skipif(
+    OS_VERSION not in ALLOWED_BCI_REPO_OS_VERSIONS,
     reason="LTSS containers are known to be non-functional with BCI_repo ",
 )
 @pytest.mark.parametrize(
